0564da3af37a0e894321f2a565991468f7eb04b9
JMarquee85 <joshmarcus85@gmail.com>
2022-12-14 15:22:38 -0700

    Some organization and added a couple new items.

--- packages.sh
+++ packages.sh
@@ -14,6 +14,9 @@ whois \
 htop \
 gparted \
 terminator \
+python3-dev \
+python3-setuptools \
+dnsutils \
 
 
 # Snaps. 
@@ -28,16 +31,16 @@ sudo apt -y install ./espanso-debian-x11-amd64.deb
 espanso service register
 espanso start
 
-# thefuck
-sudo apt install python3-dev python3-setuptools
-pip3 install thefuck --user
-
 # chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-# bpytop
-sudo python3-pip install bpytop
+# pip packages
+sudo python3-pip install \
+bpytop \
+thefuck --user \
+virtualenv --user \
+
 
 # append typical shortcuts to bashrc
 echo -e "# My aliases\nalias get'sudo apt-get -y install'\nalias upd='sudo apt-get -y update;sudo apt-get -y upgrade'\nalias p='python3'" >> ~/.bashrc
