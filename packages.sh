0f470ff176e278d62c022bb78ad50e98a4f944a8
Josh Marcus <joshmarcus85@gmail.com>
2023-11-07 08:29:47 -0700

    Added docker

--- packages.sh
+++ packages.sh
@@ -20,7 +20,16 @@ dnsutils \
 zoom \
 bluetoothd \
 tcpdump \
-tshark
+tshark 
+
+# docker
+sudo apt -y install apt-transport-https ca-certificates curl gnupg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo -y apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker ${USER}
+
 
 # Snaps. 
 # sudo snap -y install \
